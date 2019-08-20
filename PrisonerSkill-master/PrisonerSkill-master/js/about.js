9e28ee53a29b6cccb55432785dcb29f60469bdcd
Trevor Thompson <trevthom18@gmail.com>
2019-08-19 22:29:19 -0400

    Refactored, but added no new functionality

--- PrisonerSkill-master/PrisonerSkill-master/js/about.js
+++ PrisonerSkill-master/PrisonerSkill-master/js/about.js
@@ -1,15 +1,16 @@
 var aboutTrevor = document.addEventListener('click', () => {
 
-    let trevorTag = document.getElementByClassName('.about-trevor');
+    let trevorTag = document.querySelector('.about-trevor');
     alert("The 'about-trevor' element has been clicked on!");
 
 });
 
 var imageInspection = document.addEventListener('click', () => {
 
-    let trevorTag = document.getElementByClassName('img-display');
+    let trevorTag = document.querySelector('trevor-flexer');
     alert("The 'img-display' element has been clicked on!");
 
 });
 
 // Hmm, need to investigate how the event element is being reference because it looks like it 
+// Hmm, according to my research I should need this stuff to trigger displaying a text box, but I don't understand it, so I might do it this way.
