8af825648fbebff87a0586edb02199bf53f01c77
Trevor Thompson <trevthom18@gmail.com>
2019-08-24 07:02:28 -0400

    Developing Javascript functions to handle about me content, not working correctly yet though

--- PrisonerSkill-master/PrisonerSkill-master/js/about.js
+++ PrisonerSkill-master/PrisonerSkill-master/js/about.js
@@ -1,16 +1,47 @@
-var aboutTrevor = document.addEventListener('click', () => {
+const aboutMeData = [
+    {
+        name: "Trevor",
+        description: "I'm Trevor. DMV area native. System Engineer, business man, and by father, by day, and Full Stack student at night",
+        twitter:"twitter.com/earthboundtrev",
+        github: "github.com/tthompson2",
+    },
+    {
+        name: 'Ray',
+        description: "Full Stack student at Lambda School",
+        twitter: "twitter.com/lambdaschool",
+        github: "github.com/ray-aguilar",
+    },
 
+    {
+        name: "Dallas",
+        description: "Web development student at Lambda School",
+        twitter: "twitter.com/lambdaschool",
+        github: "github.com/dallas-james",
+    },
+
+    {
+        name: "Aaron",
+        description: "Web development student at Lambda School",
+        twitter: "twitter.com/lambdaschool",
+        github: "github.com/aaron-troska"
+    }
+];
+
+var aboutTrevor = document.addEventListener('click', (aboutMeData) => {
+
+    
     let trevorTag = document.querySelector('.about-trevor');
-    alert("The 'about-trevor' element has been clicked on!");
+    
+   var filterFunction = aboutMeData.filter = (des) => {
+
+      console.log(des.description);
+
+    };
+
+    filterFunction(aboutMeData);
 
 });
 
-var imageInspection = document.addEventListener('click', () => {
-
-    let trevorTag = document.querySelector('trevor-flexer');
-    alert("The 'img-display' element has been clicked on!");
-
-});
 
 // Hmm, need to investigate how the event element is being reference because it looks like it 
 // Hmm, according to my research I should need this stuff to trigger displaying a text box, but I don't understand it, so I might do it this way.
